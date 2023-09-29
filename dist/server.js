0d80681babc3da72e0ea6842482d47999f2fb324
Benjamin Lee <benjaminlee0727@gmail.com>
2023-09-29 12:55:50 -0700

    import stock data

--- dist/server.js
+++ dist/server.js
@@ -5,8 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const ws_1 = __importDefault(require("ws"));
+const fs_1 = __importDefault(require("fs"));
 const app = (0, express_1.default)();
 const server = app.listen(8080, () => {
     console.log('Server listening on port 8080');
 });
 const wss = new ws_1.default.Server({ server });
+const stockData = JSON.parse(fs_1.default.readFileSync('./stock_list.json', 'utf-8'));
+console.log(stockData);
--- server.ts
+++ server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import WebSocket from 'ws';
+import fs from 'fs';
+import generatePrice from './PriceSimulator';
 
 const app = express();
 const server = app.listen(8080, () => {
   console.log('Server listening on port 8080');
 });
-
 const wss = new WebSocket.Server({ server });
+const stockData = JSON.parse(fs.readFileSync('./stock_list.json', 'utf-8'));
+console.log(stockData)
