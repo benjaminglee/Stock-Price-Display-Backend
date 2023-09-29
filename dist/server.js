f83cad70d28932a03111e8c597d0b146fb8b8dc3
Benjamin Lee <benjaminlee0727@gmail.com>
2023-09-29 13:44:15 -0700

    update stock prices and send messages to connected clients

--- dist/server.js
+++ dist/server.js
@@ -6,10 +6,34 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const ws_1 = __importDefault(require("ws"));
 const fs_1 = __importDefault(require("fs"));
+const PriceSimulator_1 = __importDefault(require("./PriceSimulator"));
 const app = (0, express_1.default)();
 const server = app.listen(8080, () => {
     console.log('Server listening on port 8080');
 });
 const wss = new ws_1.default.Server({ server });
 const stockData = JSON.parse(fs_1.default.readFileSync('./stock_list.json', 'utf-8'));
-console.log(stockData);
+const updatedStockPrices = {};
+const updateInterval = 100;
+const connectedClients = new Set();
+wss.on('connection', (ws) => {
+    connectedClients.add(ws);
+    console.log('Client connected');
+    ws.on('close', () => {
+        connectedClients.delete(ws);
+        console.log('Client disconnected');
+    });
+});
+setInterval(() => {
+    for (const symbol in stockData) {
+        if (stockData.hasOwnProperty(symbol)) {
+            updatedStockPrices[symbol] = (0, PriceSimulator_1.default)(symbol);
+        }
+    }
+    console.log(updatedStockPrices);
+    //loop through connections and send message to clients
+    for (const ws of connectedClients) {
+        const message = JSON.stringify(updatedStockPrices);
+        ws.send(message);
+    }
+}, updateInterval);
--- server.ts
+++ server.ts
@@ -13,6 +13,29 @@ const stockData = JSON.parse(fs.readFileSync('./stock_list.json', 'utf-8'));
 const updatedStockPrices: { [symbol: string]: number } = {};
 const updateInterval = 100;
 
+const connectedClients: Set<WebSocket> = new Set();
+
+wss.on('connection', (ws) => {
+  connectedClients.add(ws);
+  console.log('Client connected');
+
+  ws.on('close', () => {
+    connectedClients.delete(ws);
+    console.log('Client disconnected');
+  });
+});
+
 setInterval(() => {
-  for(const symbol in )
+  for(const symbol in stockData) {
+    if(stockData.hasOwnProperty(symbol)) {
+      updatedStockPrices[symbol] = generatePrice(symbol);
+    }
+  }
+  console.log(updatedStockPrices);
+  //loop through connections and send message to clients
+  //TODO test connectino
+  for (const ws of connectedClients) {
+    const message = JSON.stringify(updatedStockPrices);
+    ws.send(message);
+  }
 }, updateInterval)
